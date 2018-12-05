d18145a3f51bcdd9f57b3d267012c124496e22fe
winkie224 <leanthony104@yahoo.com>
2018-12-04 20:34:26 -0600

    connected to database

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.set("view engine", "handlebars");
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
+//require("./routes/post-api-routes.js")(app);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
