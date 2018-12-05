6f5f767d98b78b829aa49ee5e5788e2d1a29c22d
winkie224 <leanthony104@yahoo.com>
2018-12-04 21:32:25 -0600

    Removed Path for Travis #14

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,12 +4,11 @@
 
 // Dependencies
 // // =============================================================
-const path = require("path");
 
 // // Routes
 // // =============================================================
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        res.render();;
+        res.render("index");
     });
 };
