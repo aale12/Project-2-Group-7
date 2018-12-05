5a3a7ba0a176354c89c265a15ff252f07db03a00
winkie224 <leanthony104@yahoo.com>
2018-12-04 18:01:52 -0600

    Models Added #14

--- models/events.js
+++ models/events.js
@@ -1,20 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
-    let Update = sequelize.define("Update", {
+    let Event = sequelize.define("Event", {
         user: DataTypes.STRING,
         editable: DataTypes.BOOLEAN,
         description: DataTypes.STRING,
         comments: DataTypes.INTEGER
     });
 
-    Update.associate = function(models) {
-        Update.belongsTo(models.Timeline, {
+    Event.associate = function(models) {
+        //Each Event has only One Timeline
+        Event.belongsTo(models.Timeline, {
             foreignKey: {
                 allowNull: false
             }
         });
-        Update.hasMany(models.Comment, {
+        //Each Event can have many comments
+        Event.hasMany(models.Comment, {
             onDelete: "cascade"
         });
     };
-    return Update;
+    return Event;
 };
