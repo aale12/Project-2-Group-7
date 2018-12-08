d5adf095071f00a3026591d1a2eb95e5315b527c
Vu Le <vuleuml2011@gmail.com>
2018-12-08 13:42:34 -0600

    come on

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -7,12 +7,17 @@
 
 // // Routes
 // // =============================================================
-module.exports = function(app) {
-    app.get("/", function(req, res) {
+module.exports = function (app) {
+    app.get("/", function (req, res) {
         res.render("index");
     });
 
-    app.get("/signup", function(req, res) {
+    app.get("/signup", function (req, res) {
         res.render("signup");
     });
+
+    app.get("/dashboard/charts", function (req, res) {
+        res.render("charts");
+    });
 };
+
